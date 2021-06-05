4986eb1e27aeb11b6660089dfeee78ed6dac1137
iharriott11 <ianharriott11@gmail.com>
2021-06-05 11:59:43 -0600

    added table and button to equipment screen

--- src/app/equipments/equipment/equipment.component.ts
+++ src/app/equipments/equipment/equipment.component.ts
@@ -1,7 +1,9 @@
 import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { QuoteService } from '../../../app/shared/quote.service';
+import { AddEquipmentComponent } from '../add-equipment/add-equipment.component';
 
 @Component({
   selector: 'app-equipment',
@@ -11,7 +13,8 @@ import { QuoteService } from '../../../app/shared/quote.service';
 export class EquipmentComponent implements OnInit {
 
   constructor(public quoteService: QuoteService,
-    private router: Router) { }
+    private router: Router,
+    private dialog: MatDialog) { }
 
   ngOnInit(): void {
     if(this.router.url.indexOf('quote') < 0){
@@ -19,6 +22,15 @@ export class EquipmentComponent implements OnInit {
       this.quoteService.hideButton = true;
     }
 
+  }
+
+  addEquipment(eventIndex, quoteNo) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.disableClose = true;
+    dialogConfig.width = '50%';
+    dialogConfig.data = { eventIndex, quoteNo };
+    this.dialog.open(AddEquipmentComponent, dialogConfig);
   }
 
 }
